73a2cd7be8e78581df359640cd2ca1bdbaf9b9e2
Dominik R. Laetsch <dominik.laetsch@gmail.com>
2014-03-13 20:22:07 +0000

    Implemented color histograms and sequential plotting

--- tagc_plot.py
+++ tagc_plot.py
@@ -1,12 +1,15 @@
 from __future__ import division
 import numpy as np
 import matplotlib as mat
-mat.use('Agg')
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from matplotlib.ticker import NullFormatter
 import sys
 
+
+# define how many phyla should be plotted
+max_phylum_plot = int(sys.argv[2])
+fig_format = 'png'
 almost_black = '#262626'
 nullfmt = NullFormatter()         # no labels
 
@@ -38,10 +41,9 @@ rect_histx = [left, bottom_h, width, 0.2]
 rect_histy = [left_h, bottom, 0.2, height]
 
 # Setting up plots and axes
-plt.ioff()
-plt.figure(figsize=(20,20))
+plt.figure(1, figsize=(20,20))
 axScatter = plt.axes(rect_scatter, axisbg='#F0F0F5', yscale = 'log')
-plt.suptitle(blob_file, fontsize=25)
+plt.suptitle(blob_file, fontsize=25, verticalalignment='bottom')
 axScatter.set_xlabel("GC", fontsize=25)
 axScatter.set_ylabel("Cov", fontsize=25)
 axScatter.grid(True, which="major", lw=2., color='#ffffff', linestyle='-') 
@@ -63,8 +65,6 @@ axHisty.set_axisbelow(True)
 # put colormap in colors 
 colors = cm.get_cmap(name='Set2')
 
-# define how many phyla should be plotted
-max_phylum_plot = 7
 i = 0
 print "Plotting :"
 for phylum in sorted_phyla:
@@ -75,27 +75,32 @@ for phylum in sorted_phyla:
 		y_i = y[cond]
 		hex_color = mat.colors.rgb2hex(colors(1.0 * (i/max_phylum_plot)))
 		if phylum != 'Not annotated':
+			top_bins = np.arange(0, 1, 0.01)
+			axHistx.hist(x_i,  color = hex_color, bins=top_bins)
+			right_bins = np.logspace(-1, 10, 500, base=10.0)
+			axHisty.hist(y_i, color = hex_color, bins=right_bins, histtype='bar', orientation='horizontal')
 			axScatter.scatter(x_i, y_i, color= hex_color, s=65, lw = 0.5, alpha=1, edgecolor=almost_black, label=phylum + " (" + str(phylum_count[phylum]) + ")")
 			i += 1
 		else:
+			top_bins = np.arange(0, 1, 0.01)
+			axHistx.hist(x_i,  color = '#d3d3d3', bins=top_bins)
+			right_bins = np.logspace(-1, 10, 500, base=10.0)
+			axHisty.hist(y_i, color = '#d3d3d3', bins=right_bins, histtype='bar', orientation='horizontal')
 			axScatter.scatter(x_i, y_i, color='#d3d3d3', s=25, lw = 0.1, alpha=0.5, edgecolor=almost_black, label=phylum + " (" + str(phylum_count[phylum]) + ")")
 			i += 1
+		axScatter.legend(loc=1, fontsize=25, scatterpoints=1) 
+		plt.savefig(blob_file + '_hist_' + str(i) + '.png', format=fig_format)
 		print " [Done]"
 	else:
 		break
 
 sys.stdout.write("Plotting histograms")
-top_bins = np.arange(0, 1, 0.01)
-axHistx.hist(x,  color = 'lightgrey', bins=top_bins)
-right_bins = np.logspace(-1, 10, 500, base=10.0)
-axHisty.hist(y, color = 'lightgrey', bins=right_bins, orientation='horizontal')
-for label in axHisty.get_xticklabels():
-    label.set_rotation('vertical')
+#for label in axHisty.get_xticklabels():
+#    label.set_rotation('vertical')
 axScatter.legend(loc=1, fontsize=25, scatterpoints=1)
 print " [Done]"
 
-fig_format = 'png'
 sys.stdout.write("Saving file " + blob_file + "." + fig_format)
-plt.savefig(blob_file + '.png', format=fig_format)
+plt.savefig(blob_file + '_hist.png', format=fig_format)
 print " [Done]"
 #plt.show()
