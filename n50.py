f971390b425e628abba3257a3729791266de2b9c
Dominik R. Laetsch <dominik.laetsch@gmail.com>
2014-03-17 16:01:03 +0000

    Fixed so that it works with real files

--- n50.py
+++ n50.py
@@ -3,16 +3,33 @@ import random
 from __future__ import division
 
 
-def n50_2(array):
-	sorted_array=sorted(array)
-	temp_array = []
-	for contig_length in sorted_array:
-		extension = [int(contig_length)] * int(contig_length)
-		temp_array.extend(extension)
-	length = len(temp_array)
-	even = (0 if length % 2 else 1) + 1
-	half = (length - 1) / 2
-	return sum(temp_array[half:half + even]) / int(even)
+# def n50_2(array):
+# 	sorted_array=sorted(array)
+# 	temp_array = []
+# 	for contig_length in sorted_array:
+# 		extension = [int(contig_length)] * int(contig_length)
+# 		temp_array.extend(extension)
+# 	length = len(temp_array)
+# 	even = (0 if length % 2 else 1) + 1
+# 	half = (length - 1) / 2
+# 	return sum(temp_array[half:half + even]) / int(even)
+
+def read_fasta_len_to_list (filename):
+	with open(filename) as fh:
+		fasta_len_list = []
+		contig = ''
+		seq = ''
+		for line in fh:
+		    line = line.rstrip("\n")
+		    if line.startswith(">"):
+		    	if (contig):
+		        	fasta_len_list.append(len(seq))
+		        seq = ''
+		        contig = line[1:]
+		    else:
+		        seq += line
+		fasta_len_list.append(len(seq))
+	return fasta_len_list
 
 def n50(array):
 	cum_len = 0
@@ -32,14 +49,15 @@ def n50(array):
 			break
 	return N50
 
-random_list = [int(10000*random.random()) for i in xrange(100000)]
+filename = sys.argv[1]
+fasta_len_list = read_fasta_len_to_list(filename)
 start = time.time()
-print n50(random_list)
+print n50(fasta_len_list)
 end = time.time()
 elapsed = end - start
-print "Time taken: ", elapsed, "seconds."
-start = time.time()
-print n50_2(random_list)
-end = time.time()
-elapsed = end - start
-print "Time taken: ", elapsed, "seconds."
+print filename + " : Time taken ", elapsed, "seconds."
+#start = time.time()
+#print n50_2(random_list)
+#end = time.time()
+#elapsed = end - start
+#print "Time taken: ", elapsed, "seconds."
