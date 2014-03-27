1f951f4020a1bad53527c125d11745f76049ae68
Dominik R Laetsch <dominik.laetsch@gmail.com>
2014-03-27 14:30:23 +0000

    usage

--- fragmentizer.pl
+++ fragmentizer.pl
@@ -2,29 +2,31 @@
 use strict;
 use warnings;
 use Getopt::Long qw(:config pass_through no_ignore_case);
+
 # This creates $fragment_len long fragments of contigs
 use Data::Dumper;
 
-my $fragment_len ;
-my $len_threshold ;
+my $fragment_len;
+my $len_threshold;
 my $assembly_file;
 
-GetOptions (
-    "file=s" => \$assembly_file,
+GetOptions(
+    "file=s"      => \$assembly_file,
     "threshold=i" => \$len_threshold,
-    "len=i" => \$fragment_len
+    "len=i"       => \$fragment_len
 );
-$len_threshold = (0.5) unless $len_threshold;
-$fragment_len = (1000) unless $fragment_len;
+$len_threshold = (0.5)  unless $len_threshold;
+$fragment_len  = (1000) unless $fragment_len;
 
-unless $assembly_file die "\nUsage: fragmentizer.pl -f contigs1.fa contigs2.fa -l 500 -t 0.25\n
-\t-f Assembly files to be split\n
-\t-l length at which to split the sequences [default: 1000]\n
-\t-t percentage of length at which the end of the sequence gets split into a new sequence [default: 0.5].\n
-\t\te.g at l = 1000, t = 0.5 : a 1499 nt long sequence does not get split and a 1501 nt long sequence gets split into two sequences (1000nt and 501nt)\n
-\tIf the program encounters more than 10 consecutive N's it replaces them with 10 N's \n"
+unless $assembly_file die
+    "\nUsage: fragmentizer.pl -f contigs1.fa contigs2.fa -l 500 -t 0.25\n
+    \t-f Assembly files to be split\n
+    \t-l length at which to split the sequences [default: 1000]\n
+    \t-t percentage of length at which the end of the sequence gets split into a new sequence [default: 0.5].\n
+    \t\te.g at l = 1000, t = 0.5 : a 1499 nt long sequence does not get split and a 1501 nt long sequence gets split into two sequences (1000nt and 501nt)\n
+    \tIf the program encounters more than 10 consecutive N's it replaces them with 10 N's \n";
 
-open IN, "<$assembly_file" || die "Can't read\n";
+    open IN, "<$assembly_file" || die "Can't read\n";
 
 my $header = '';
 my $seq    = '';
@@ -35,7 +37,8 @@ my @array;
 while ( my $line = <IN> ) {
     if ( $line =~ /^>(\S+)\n/ ) {
         if ( $header ne '' ) {
-            $seq =~ s/N{11,}/NNNNNNNNNN/g; # replace ocurrence of more than 10 N's with 10 N's
+            $seq =~ s/N{11,}/NNNNNNNNNN/g
+                ;    # replace ocurrence of more than 10 N's with 10 N's
             push @array,
                 { 'header' => $header, 'seq' => $seq, 'len' => $len };
         }
