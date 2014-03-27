16818bd183ce87100bd32228f03e54ef0837f801
Dominik R Laetsch <dominik.laetsch@gmail.com>
2014-03-27 15:08:32 +0000

    Added verbose output

--- fragmentizer.pl
+++ fragmentizer.pl
@@ -8,11 +8,13 @@ use Data::Dumper;
 my $fragment_len ;
 my $len_threshold ;
 my $assembly_file = '';
+my $verbose;
 
 GetOptions (
     "file=s" => \$assembly_file,
     "threshold=i" => \$len_threshold,
-    "len=i" => \$fragment_len
+    "len=i" => \$fragment_len,
+    "verbose" => \$verbose
 );
 $len_threshold = (0.5) unless $len_threshold;
 $fragment_len = (1000) unless $fragment_len;
@@ -23,6 +25,7 @@ Usage: fragmentizer.pl -f assembly_file.fa [-l] [-t]
 -l length at which to split the sequences [default: 1000]
 -t percentage of length at which the end of the sequence gets split into a new sequence [default: 0.5].
    e.g at l = 1000, t = 0.5 : a 1500 nt long sequence does not get split and a 1501 nt long sequence gets split into two sequences (1000nt and 501nt) 
+-v verbose
 # If the program encounters more than 10 consecutive N's it replaces them with 10 N's 
 USAGE
 unless ($assembly_file ne '');
@@ -61,14 +64,13 @@ for my $i ( 0 .. $#array ) {
     my $header                = $array[$i]{'header'};
     my $seq                   = $array[$i]{'seq'};
     my $len                   = $array[$i]{'len'};
-    #print $header." ".$len."\n";
     my $modulo                = $len % $fragment_len;
     my $number_of_full_chunks = ( $len - $modulo ) / $fragment_len;
     my @subsequences          = ( $seq =~ /(.{1,$fragment_len})/g );
-
+    my $print_string = $header." ".$len."\n";
     if ( $number_of_full_chunks == 0 ) {
         print OUT ">" . $header . "_1\n" . $subsequences[0] . "\n";
-        #print length($subsequences[0])."\n";
+        $print_string .= "_1\t".length($subsequences[0])."\n";
         next;
     }
     if ( $modulo > 0 && ( $modulo / $fragment_len ) <= $len_threshold ) {
@@ -79,7 +81,10 @@ for my $i ( 0 .. $#array ) {
             . $header . "_"
             . ( $j + 1 ) . "\n"
             . $subsequences[$j] . "\n";
-        #print length($subsequences[$j])."\n";
+        $print_string .= ($j+1)."\t".length($subsequences[$j])."\n";
+    }
+    if ($verbose){
+        print $print_string;
     }
 }
 close OUT;
