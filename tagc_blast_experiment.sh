9cd10e940b1b225b80b56cf4073276bef724ac72
Dominik R Laetsch <dominik.laetsch@gmail.com>
2014-05-19 10:29:07 +0100

    test

--- tagc_blast_experiment.sh
+++ tagc_blast_experiment.sh
@@ -6,9 +6,11 @@
 contig_file = $1 
 out_format = '6 qseqid staxids std sskingdom sscinames sblastnames stitle'
 
-sge_blast blastn -task megablast -query $contig_file -db $BLASTDB/nt -out $contig_file.megablast.out -outfmt $out_format -num_jobs 200 -evalue 1e-5 -max_target_seqs 1 
-sge_blast blastn -task dc-megablast -query $contig_file -db $BLASTDB/nt -out $contig_file.dc-megablast.out -outfmt $out_format -num_jobs 200 -evalue 1e-5 -max_target_seqs 1 
-sge_blast blastn -task blastn -query $contig_file -db $BLASTDB/nt -out $contig_file.blastn.out -outfmt $out_format -num_jobs 200 -evalue 1e-5 -max_target_seqs 1 
+blastn -task megablast -query $contig_file -db $BLASTDB/nt -out $contig_file.megablast.out -outfmt $out_format -num_jobs 200 -evalue 1e-5 -max_target_seqs 1 
+
+# sge_blast blastn -task megablast -query $contig_file -db $BLASTDB/nt -out $contig_file.megablast.out -outfmt $out_format -num_jobs 200 -evalue 1e-5 -max_target_seqs 1 
+# sge_blast blastn -task dc-megablast -query $contig_file -db $BLASTDB/nt -out $contig_file.dc-megablast.out -outfmt $out_format -num_jobs 200 -evalue 1e-5 -max_target_seqs 1 
+# sge_blast blastn -task blastn -query $contig_file -db $BLASTDB/nt -out $contig_file.blastn.out -outfmt $out_format -num_jobs 200 -evalue 1e-5 -max_target_seqs 1 
 
 # program that then takes the blast output files and reports for each contig the kingdom and species of best hit and eval, length and gc 
 # make a blobplot for each blast
