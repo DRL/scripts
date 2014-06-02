d773e2c98f39001bfcadf887ac87091ac85bb797
Dominik R Laetsch <dominik.laetsch@gmail.com>
2014-06-02 15:55:25 +0100

    work in progress

--- mask_blastdb_by_taxid.py
+++ mask_blastdb_by_taxid.py
@@ -7,9 +7,38 @@ Author  : Dominik R. Laetsch, dominik.laetsch at gmail dot com
 Version : 0.1
 """
 
-import argparse
-from Bio import Entrez
+import os, argparse
+from subprocess import call
 
+class db(object):
+	def __init__(self, filename):
+		self.filename = filename
+		if (self.found):
+			call(["module load blast"])
+			print call(["blastdbcmd -db" + self.filename + " -info"])
+		else:
+			print "Database " + self.filename + " can't be found" 
+
+	def found(self):
+		return os.path.isfile(self.filename)
+
+#class blastdb(db):
+#	def get_file(self):
+#		if self.type = 'n'
+#			return 
+
+#def choose_taxid_dmp(blastdb_type):
+#	if (blastdb_type == 'p'):
+	# 	db
+	# 	try:
+
+	# 		gi_taxid_nucl_dmp = '$BLASTDB/gi_taxid_nucl.dmp' 
+	# 	except IOError as ex:
+	# 		print "File $BLASTDB/gi_taxid_nucl.dmp could not be found" 
+	# 	gi_taxid_dmp = gi_taxid_prot_dmp
+	# else:
+	# 	gi_taxid_dmp = gi_taxid_nucl_dmp
+	# return gi_taxid_dmp
 
 if __name__ == "__main__":
 
@@ -21,18 +50,20 @@ if __name__ == "__main__":
 		prog='mask_blastdb_by_taxid',
 		usage = '%(prog)s -db -type -taxid [-merge] [-h]',
 		add_help=True)
-	parser.add_argument('-db', metavar = 'blastdb', help='input blastdb')
-	parser.add_argument('-type', metavar = 'type', default="n", help='blastdb type ("p" or "n", default: "n")')
+	parser.add_argument('-db', metavar = 'blastdb_in', help='blastdb input')
+	parser.add_argument('-out', metavar = 'blastdb_out', help='blastdb output prefix')
+	parser.add_argument('-seq', metavar = 'seq_type', default="n", help='blastdb type ("p" or "n", default: "n")')
 	parser.add_argument('-taxids', metavar = 'taxids' , default=[], type = int, nargs='+', help='TaxIDs for which BLASTdbs should be generated') 
 	parser.add_argument('-merge', action='store_true' , help='Set flag for merging ') 
+
 	args = parser.parse_args()
 
-	blastdb, blastdb_type, taxids, merge_flag = args.blastdb, args.type, args.taxids, args.merge
+	blastdb_in, seq_type, taxids, merge_flag, out_prefix = args.db, args.st, args.taxids, args.merge, args.out
 
-	gi_taxid_nucl_dmp = '$BLASTDB/ 
-	gi_taxid_prot_dmp = 
-	if (taxids):
-		for taxid in taxids:
-		
-	
+	gi_taxid_nucl_dmp = db('$BLASTDB/gi_taxid_nucl.dmp', 'n') 
+	gi_taxid_prot_dmp = db('$BLASTDB/gi_taxid_prot.dmp', 'p')
+	blastdb = db(blastdb_in)
+	#nr = db('$BLASTDB/nr', 'p')
 
+	#gi_taxid_dmp = choose_taxid_dmp(blastdb_type) 
+	#print gi_taxid_dmp
